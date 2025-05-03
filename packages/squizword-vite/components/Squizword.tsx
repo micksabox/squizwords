48f62b6ca2b7c2314dfa30d306e8b29e6d3cdc8b
Michael Nolivos <michaelnolivos@gmail.com>
2025-05-03 15:29:30 -0400

    feat: refactor Squizword component to load crossword data from JSON file and streamline data structure

--- packages/squizword-vite/components/Squizword.tsx
+++ packages/squizword-vite/components/Squizword.tsx
@@ -10,6 +10,7 @@ import { useOffChainVerification } from '../hooks/useOffChainVerification.jsx';
 import { GuessGrid } from '../../mycrossword/lib/types.js';
 import { getGameClueGuesses } from '../utils/gamegrid.js';
 import { toast } from 'react-toastify';
+import crosswordData from '../crosswords/linktotheproofs.json' assert { type: "json" };
 
 function Component() {
   const [input, setInput] = useState<{ word: string; solutionHash: string } | undefined>();
@@ -28,90 +29,7 @@ function Component() {
   //   setInput({ word: word.value, solutionHash: solutionHash.value });
   // };
 
-  const data: GuardianCrossword = {
-    id: 'simple/1',
-    number: 1,
-    name: 'Simple Crossword #1',
-    date: 1542326400000,
-    entries: [
-      {
-        id: '1-across',
-        number: 1,
-        humanNumber: '1',
-        clue: 'Toy on a string (2-2)',
-        direction: 'across',
-        length: 4,
-        group: ['1-across'],
-        position: { x: 0, y: 0 },
-        separatorLocations: {
-          '-': [2],
-        },
-        // solution: 'YOYO',
-      },
-      {
-        id: '4-across',
-        number: 4,
-        humanNumber: '4',
-        clue: 'Have a rest (3,4)',
-        direction: 'across',
-        length: 7,
-        group: ['4-across'],
-        position: { x: 0, y: 2 },
-        separatorLocations: {
-          ',': [3],
-        },
-        solution: 'LIEDOWN',
-      },
-      {
-        id: '1-down',
-        number: 1,
-        humanNumber: '1',
-        clue: 'Colour (6)',
-        direction: 'down',
-        length: 6,
-        group: ['1-down'],
-        position: { x: 0, y: 0 },
-        separatorLocations: {},
-        solutionPoseidonHash: '0x26fe5c527f143708960f465deda2d7ea97f585dffaa627069c44ef80b8b4730',
-      },
-      {
-        id: '2-down',
-        number: 2,
-        humanNumber: '2',
-        clue: 'Bits and bobs (4,3,4)',
-        direction: 'down',
-        length: 7,
-        group: ['2-down', '3-down'],
-        position: { x: 3, y: 0 },
-        separatorLocations: {
-          ',': [4, 7],
-        },
-        solution: 'ODDSAND',
-      },
-      {
-        id: '3-down',
-        number: 3,
-        humanNumber: '3',
-        clue: 'See 2',
-        direction: 'down',
-        length: 4,
-        group: ['2-down', '3-down'],
-        position: {
-          x: 6,
-          y: 1,
-        },
-        separatorLocations: {},
-        solution: 'ENDS',
-      },
-    ],
-    solutionAvailable: false,
-    dateSolutionAvailable: 1542326400000,
-    dimensions: {
-      cols: 8,
-      rows: 8,
-    },
-    crosswordType: 'quick',
-  };
+  const data: GuardianCrossword = crosswordData as GuardianCrossword;
 
   return (
     <>
@@ -192,7 +110,7 @@ function Component() {
           disableLetterChecks={true}
           disableGridChecks={true}
           allowMissingSolutions={true}
-          id="crossword-1"
+          id={data.id}
           data={data}
         />
       </div>
